74a81e4a542b433c1c800eb1d9b43470719ee91d
Mystic Quest <l1skou@gmail.com>
2020-06-05 00:13:23 +0300

    send to sql

--- call.php
+++ call.php
@@ -1,4 +1,21 @@
 <?php
-$info=$_POST['fname'];
-echo 'Added = '.$info;
+$first=$_POST['fname'];
+$last=$_POST['lname'];
+$age=$_POST['age'];
+
+$con = new mysqli("localhost", "root", "", "ajaxtest");
+$sql = "INSERT INTO `names` (`first`, `last`, `age`) VALUES ('.$first.', '$.last.', '$.age.');";
+
+if($con->ping()){
+    // echo "connected";
+}
+
+if($con->query($sql) === TRUE){
+    echo "Creacted entry";
+}
+else{
+    echo "Connection Error: ".$sql."<br>".$con->error;
+}
+
+echo 'First = '.$first;
 ?>
